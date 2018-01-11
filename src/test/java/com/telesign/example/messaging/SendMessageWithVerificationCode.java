f247cb6f8e4b375074176a48cb9c9a33d83de3cb
MicahLS <35348370+MicahLS@users.noreply.github.com>
2018-01-11 13:39:26 -0700

    Update SendMessageWithVerificationCode.java

--- src/test/java/com/telesign/example/messaging/SendMessageWithVerificationCode.java
+++ src/test/java/com/telesign/example/messaging/SendMessageWithVerificationCode.java
@@ -25,3 +25,34 @@ public class SendMessageWithVerificationCode {
         }
     }
 }
+  package com.telesign.example.messaging;
+
+  import com.telesign.MessagingClient;
+  import com.telesign.RestClient;
+  import com.telesign.Util;
+
+  public class SendMessageWithVerificationCode {
+
+      public static void main(String[] args) {
+
+          String customerId = "2CCC4B4A-9111-4802-A302-10156BBC5034";
+          String apiKey = "*********************************";
+
+          String phoneNumber = "15052612036";
+          String verifyCode = Util.randomWithNDigits(5);
+
+          String message = String.format("Your code is %s", verifyCode);
+          String messageType = "OTP";
+
+
+
+          try {
+
+              MessagingClient messagingClient = new MessagingClient(customerId, apiKey);
+              RestClient.TelesignResponse telesignResponse = messagingClient.message(phoneNumber, message, messageType, null);
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+      }
+  }
+  
