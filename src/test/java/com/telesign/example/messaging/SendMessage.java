1efce168193820bd6df4ddb47e1a6bf3fe71bc14
MicahLS <35348370+MicahLS@users.noreply.github.com>
2018-01-11 14:07:39 -0700

    Update SendMessage.java

--- src/test/java/com/telesign/example/messaging/SendMessage.java
+++ src/test/java/com/telesign/example/messaging/SendMessage.java
@@ -1,24 +1,32 @@
-package com.telesign.example.messaging;
 
-import com.telesign.MessagingClient;
-import com.telesign.RestClient;
+     package com.telesign.example.messaging;
 
-public class SendMessage {
+  import com.telesign.MessagingClient;
+  import com.telesign.RestClient;
+  import com.telesign.Util;
 
-    public static void main(String[] args) {
+  public class SendMessageWithVerificationCode {
 
-        String customerId = "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
-        String apiKey = "EXAMPLE----TE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
+      public static void main(String[] args) {
 
-        String phoneNumber = "phone_number";
-        String message = "You're scheduled for a dentist appointment at 2:30PM.";
-        String messageType = "ARN";
+          String customerId = "2CCC4B4A-9111-4802-A302-10156BBC5034";
+          String apiKey = "*********************************";
 
-        try {
-            MessagingClient messagingClient = new MessagingClient(customerId, apiKey);
-            RestClient.TelesignResponse telesignResponse = messagingClient.message(phoneNumber, message, messageType, null);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+          String phoneNumber = "15052612036";
+          String verifyCode = Util.randomWithNDigits(5);
+
+          String message = String.format("Your code is %s", verifyCode);
+          String messageType = "OTP";
+
+
+
+          try {
+
+              MessagingClient messagingClient = new MessagingClient(customerId, apiKey);
+              RestClient.TelesignResponse telesignResponse = messagingClient.message(phoneNumber, message, messageType, null);
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+      }
+  }
+  
